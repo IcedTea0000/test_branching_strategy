ffd5fa668fffec52a7c621ec1409a5af86f11167
Lam Ngo <ngohoanglam65@gmail.com>
2022-09-21 06:20:15 +0700

    first commit

---
+++ git.sh
@@ -0,0 +1,19 @@
+git init
+git remote add origin https://github.com/user/repo.git
+# Set a new remote
+
+git remote -v
+# Verify new remote
+
+# create new branch off current branch
+git checkout -b new_branch
+
+# create new branch off branch dev
+git checkout -b feature/feature_a dev
+
+# delete branch locally
+git branch -d localBranchName
+
+# delete branch remotely
+git push origin --delete remoteBranchName
+
